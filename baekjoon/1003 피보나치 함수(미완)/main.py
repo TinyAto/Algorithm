750bd0e345a53bfb7c75ed52c16208c5a0be00ac
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-01-27 17:56:58 +0900

    피보나치 중간저장

--- baekjoon/1003 피보나치 함수(미완)/main.py
+++ baekjoon/1003 피보나치 함수(미완)/main.py
@@ -3,23 +3,32 @@ sys.stdin = open("input.txt", "r")
 
 T = int(input())
 
-count_0 = 0
-count_1 = 0
+count = [0, 0]
 def fibonacci(n):
-    global count_0
-    global count_1
-    if (n == 0):
-        count_0 += 1
+    global dynfibo
+    global count
+    # fibonacci(0)
+    if n == 0:
+        count[0] += 1
         return 0
-    elif (n == 1):
-        count_1 += 1
+    # fibonacci(1)
+    elif n == 1:
+        count[1] += 1
         return 1
+    # fibonacci(2~)
     else:
-        return fibonacci(n-1) + fibonacci(n-2)
+        # 임시 저장공간에 있을 시 <-- 이때가 문젠데 값이 있을 경우 어떻게?
+        # 일단 N번 한번 쫙 돌고 fibonacci(N-1)의 호출 수는 (count[1] - count[0], count[0])인데..
+        if dynfibo[n - 1] != 0:
 
+            return dynfibo[n]
+        # 임시 저장공간에 없을 시
+        else:
+            dynfibo[n - 1] = fibonacci(n - 2) + fibonacci(n - 1)
+            return fibonacci(n-1) + fibonacci(n-2)
 for test_case in range(1, T + 1):
     N = int(input())
-    fibonacci(N)
-    print("{} {}".format(count_0, count_1))
-    count_0 = 0
-    count_1 = 0
+    dynfibo = [0] * N
+    fibo_N = fibonacci(N)
+    print(f"{count[0]} {count[1]}")
+    count = [0, 0]
