b9a0b9fe61714c94b32a382ced22c394e981c8f0
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-01-29 17:58:03 +0900

    백준 1022번 임시저장

--- currently_solving.py
+++ currently_solving.py
@@ -1,3 +1,8 @@
+# https://www.acmicpc.net/problem/1022
 import sys
 sys.stdin = open("input.txt", "r")
 
+r1, c1, r2, c2 = map(int, input().split())
+# 정사각현 한 변의 길이는 r2 - r1 + 1
+# 자릿수 공백으로 체우기(n자리) print(f"{num:nd}")
+# 자릿수 0으로 체우기(n자리) print(f"{num:0nd}")
