ac835be66a890a8dad6caf24c068ded6ea3cec4d
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-01-29 11:32:14 +0900

    [Bronze I] Title: 단어 공부, Time: 212 ms, Memory: 34368 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,16 @@
+input_str = input().upper()
+count_dict = {}
+for char in input_str:
+    if not (char in count_dict):
+        count_dict.setdefault(char, 1)
+    else:
+        count_dict[char] += 1
+count_list = list(count_dict.values())
+maxval = max(count_list)
+count_list.remove(maxval)
+if maxval in count_list:
+    print("?")
+else:
+    for key, value in count_dict.items():
+        if value == maxval:
+            print(key)
