20f3f47038e97f3d0f2c3af0d14df02349e499aa
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-01-29 13:16:59 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -0,0 +1,16 @@
+matrix = []
+try:
+    while True:
+        matrix.append(list(map(int, input().split())))
+except:
+    pass
+maxval = matrix[0][0]
+maxindex = [0, 0]
+for row in range(len(matrix)):
+    for col in range(len(matrix[0])):
+        if matrix[row][col] > maxval:
+            maxval = matrix[row][col]
+            maxindex = [row, col]
+
+print(maxval)
+print(maxindex[0] + 1, maxindex[1] + 1)
