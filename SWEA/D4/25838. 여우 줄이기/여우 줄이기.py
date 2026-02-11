222691cdd7b3b2efd8ffc4f309ce565b629851a6
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-02-11 14:01:33 +0900

    [D4] Title: 여우 줄이기, Time: 324 ms, Memory: 98,972 KB -BaekjoonHub

---
+++ SWEA/D4/25838. 여우 줄이기/여우 줄이기.py
@@ -0,0 +1,16 @@
+# import sys
+# sys.stdin = open("input.txt", "r")
+
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    text_n = int(input())
+    text_data = input()
+    final_string = []
+    for char in text_data:
+        final_string.append(char)
+        if final_string[-3:] == ['f', 'o', 'x']:
+            final_string.pop()
+            final_string.pop()
+            final_string.pop()
+    print(len(final_string))
