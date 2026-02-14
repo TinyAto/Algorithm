3fd8c0fcb3fbd3d96ee0db73f31829b61a7d3c71
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-02-14 18:19:22 +0900

    solved 22806 도로 건설

--- IM대비 추가문제/22806 도로 건설.py
+++ IM대비 추가문제/22806 도로 건설.py
@@ -21,18 +21,27 @@ def get_bridge_value(arr, i, j):
 T = int(input())
 for tc in range(T):
     N = int(input())
-    min_height = 20*20*4
-    min_cost = 20*20*4
+    min_height = 5 * N * N
+    min_cost = 5 * N * N
     arr = [list(map(int, input().split())) for _ in range(N)]
     for i in range(N):
         for j in range(N):
-            cost = 0
             height_lst = get_bridge_value(arr, i, j)
+            # build_height: 현재 건설 예정인 높이
+            # height: 배열 내부 요소의 높이
             for build_height in set(height_lst):
+                cost = 0
                 for height in height_lst:
                     cost += abs(height - build_height)
-                if min_cost >= cost:
-                    if min_height >= build_height:
-                        min_cost = cost
+                # 기록되어있는 최소 코스트가 현재 코스트보다 크면
+                if min_cost > cost:
+                    # 최소 코스트, 최소 높이 둘다 업데이트
+                    min_cost = cost
+                    min_height = build_height
+                # 기록되어있는 최소 코스트가 현재 코스트보다 크면
+                elif min_cost == cost:
+                    # 기록되어있는 최소 높이가 현재 높이보다 크면
+                    if min_height > build_height:
+                        # 높이 최솟값 업데이트
                         min_height = build_height
-    print(f"#{tc+1} {min_cost} {min_height}")
+    print(f"#{tc + 1} {min_cost} {min_height}")
