75e7015c229f89f67acc27dd9e2d2adda7e0a3a2
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-02-16 13:41:41 +0900

    solved 22391.스위치 조작

---
+++ IM대비 추가문제/22391 스위치 조작.py
@@ -0,0 +1,25 @@
+import sys
+from inspect import currentframe, getfile
+from pkgutil import resolve_name
+
+input_text_flie_name = getfile(currentframe()).split("/")[-1][:-3] + ".txt"
+try:
+    sys.stdin = open(input_text_flie_name)
+except:
+    with open(input_text_flie_name, 'w', encoding='utf-8') as f:
+        pass
+    sys.stdin = open(input_text_flie_name)
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    arr = list(map(int, input().split()))
+    ans = list(map(int, input().split()))
+    cnt = 0
+    for i in range(len(arr)):
+        if arr[i] != ans[i]:
+            for j in range(i, i + len(arr[i:])):
+                arr[j] = int(not(arr[j]))
+            cnt += 1
+    print(f"#{tc} {cnt}")
