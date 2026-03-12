45ff588341c5171f075b558c0c0f2ce8aba0ba6e
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-03-12 11:54:17 +0900

    [D3] Title: 증가하는 사탕 수열, Time: 66 ms, Memory: 57,984 KB -BaekjoonHub

---
+++ SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
@@ -0,0 +1,15 @@
+T = int(input())
+for tc in range(1, T + 1):
+    print(f"#{tc} ", end='')
+    a, b, c = map(int, input().split())
+    ans = 0
+    if c < 3 or b < 2:
+        print(-1)
+    else:
+        while b >= c:
+            b -= 1
+            ans += 1
+        while a >= b:
+            a -= 1
+            ans += 1
+        print(ans)
