e3b2160acc2a1b1edfb51fa56b5d983f3d21c1fe
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-03-13 09:22:24 +0900

    [D3] Title: 증가하는 사탕 수열, Time: 65 ms, Memory: 50,944 KB -BaekjoonHub

--- SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
+++ SWEA/D3/20551. 증가하는 사탕 수열/증가하는 사탕 수열.py
@@ -6,10 +6,17 @@ for tc in range(1, T + 1):
     if c < 3 or b < 2:
         print(-1)
     else:
-        while b >= c:
-            b -= 1
-            ans += 1
-        while a >= b:
-            a -= 1
-            ans += 1
+        # while b >= c:
+        #     b -= 1
+        #     ans += 1
+        # while a >= b:
+        #     a -= 1
+        #     ans += 1
+        # print(ans)
+        if b >= c:
+            ans += b - (c - 1)
+            b = c - 1
+        if a >= b:
+            ans += a - (b - 1)
+            a = b - 1
         print(ans)
