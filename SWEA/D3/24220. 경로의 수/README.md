de0fafd5520e90a9a4ee2568fccd219d86214821
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-04-06 14:11:46 +0900

    [D3] Title: 경로의 수, Time: 59 ms, Memory: 50,176 KB -BaekjoonHub

---
+++ SWEA/D3/24220. 경로의 수/README.md
@@ -0,0 +1,15 @@
+# [D3] 경로의 수 - 24220 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AZWsDXh6C8fHBISP) 
+
+### 성능 요약
+
+메모리: 50,176 KB, 시간: 59 ms, 코드길이: 754 Bytes
+
+### 제출 일자
+
+2026-04-06 14:11
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/24220. 경로의 수/경로의 수.py
@@ -0,0 +1,28 @@
+def dfs(node, trace):
+    global route_cnt
+    visited[node] = True
+    if node == G:
+        trace.append(node)
+        # print(trace)
+        route_cnt += 1
+        return
+    elif len(trace) == E:
+        return
+    for adj_node in adj_lst[node]:
+        if not visited[adj_node]:
+            dfs(adj_node, trace + [node])
+            visited[adj_node] = False
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    route_cnt = 0
+    N, E = map(int, input().split())
+    adj = list(map(int, input().split()))
+    adj_lst = [[] for _ in range(N + 1)]
+    visited = [False for _ in range(N + 1)]
+    S, G = map(int, input().split())
+    for i in range(0, len(adj), 2):
+        adj_lst[adj[i]].append(adj[i + 1])
+    dfs(1,[])
+    print(f"#{tc} {route_cnt}")
