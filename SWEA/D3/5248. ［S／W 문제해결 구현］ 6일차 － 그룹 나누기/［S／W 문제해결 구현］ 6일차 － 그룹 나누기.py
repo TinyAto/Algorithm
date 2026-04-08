7ef467059d2e44810bbf089c042b749226688c5b
TinyAto <70744013+TinyAto@users.noreply.github.com>
2026-04-08 14:24:49 +0900

    [D3] Title: [S/W 문제해결 구현] 6일차 - 그룹 나누기, Time: 53 ms, Memory: 51,328 KB -BaekjoonHub

--- SWEA/D3/5248. ［S／W 문제해결 구현］ 6일차 － 그룹 나누기/［S／W 문제해결 구현］ 6일차 － 그룹 나누기.py
+++ SWEA/D3/5248. ［S／W 문제해결 구현］ 6일차 － 그룹 나누기/［S／W 문제해결 구현］ 6일차 － 그룹 나누기.py
@@ -1,3 +1,40 @@
+'''
+# DFS 풀이법
+def dfs(v):
+    visited[v] = 1
+    for w in adj_lst[v]:
+        if visited[w] == 0:
+            dfs(w)
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    temp = list(map(int, input().split()))
+    adj_lst = [[] for _ in range(N + 1)]  # 인접리스트
+    visited = [0] * (N + 1)
+    for i in range(M):
+        s, e = temp[2 * i], temp[2 * i + 1]
+        adj_lst[s].append(e)
+        adj_lst[e].append(s)
+
+    cnt = 0
+    for i in range(1, N + 1):
+        if visited[i] == 0:
+            dfs(i)
+            cnt += 1
+    print(f'#{tc} {cnt}')
+
+
+T = int(input())
+
+
+def find_set(x):
+    if parent[x] != x:
+        return find_set(parent[x])
+    else:
+        return x
+'''
 
 T = int(input())
 
